9d438e560b62693ed1f7be85fdc8c2c8019b1937
Nicholas <nrcar94@gmail.com>
2025-08-08 09:12:23 -0500

    Added type hinting

--- television.py
+++ television.py
@@ -1,22 +1,22 @@
 
-class Television():
+class Television:
     """
     Class to mimic a functioning tv
     """
 
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
     def __init__(self):
 
-        self.status = False
-        self.muted = False
-        self.volume = self.MIN_VOLUME
-        self.channel = self.MIN_CHANNEL
+        self.status: bool = False
+        self.muted: bool = False
+        self.volume: int = self.MIN_VOLUME
+        self.channel: int = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         """
         Toggles power variable for tv
         """
@@ -27,7 +27,7 @@ class Television():
         else:
             self.status = False
 
-    def mute(self):
+    def mute(self) -> None:
         """
         Toggles mute status for tv
         """
@@ -40,7 +40,7 @@ class Television():
             else:
                 self.muted = False
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         Increments the tv's channel up and goes back to the min channel if over the max
         """
@@ -53,7 +53,7 @@ class Television():
             else:
                 self.channel = self.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         Increments the tv's channel down and goes back to the max channel if under the min
         """
@@ -66,7 +66,7 @@ class Television():
             else:
                 self.channel = self.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         Increments volume up, toggles mute off if on, does not increment further if at volume max
         """
@@ -79,7 +79,7 @@ class Television():
             if self.volume < self.MAX_VOLUME:
                 self.volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         Increments the volume down,toggles mute off, does not increment further if at min volume
         """
@@ -92,7 +92,7 @@ class Television():
             if self.volume > self.MIN_VOLUME:
                 self.volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Get Televisions' Power, Channel, and volume
         :return: fstring of power channel and volume
